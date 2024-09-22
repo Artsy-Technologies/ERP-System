c407ab1dadc228d2b57275b64ff16b455baf1266
Vasudev <vasudevbinu25@gmail.com>
2024-09-22 23:10:27 +0530

    faculty commit

---
+++ client/src/components/Faculty_profile/inside_components/settings.jsx
@@ -0,0 +1,16 @@
+import '../styles/settings.css';
+
+const SettingsSidebar = () => {
+  return (
+    <div className="settings-sidebar">
+      <ul className="settings-links">
+        <li><strong>Edit Profile</strong></li>
+        <li><strong>Notification</strong></li>
+        <li><strong>Appearance</strong></li>
+        <li><strong>Help</strong></li>
+      </ul>
+    </div>
+  );
+};
+
+export default SettingsSidebar;
