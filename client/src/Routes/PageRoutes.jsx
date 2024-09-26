982e150173c0bf0ed166e2e2f9eac9ee416cc664
Prithwi <prithwi.online11@gmail.com>
2024-09-26 16:29:34 +0530

    Frontend  for project

--- client/src/Routes/PageRoutes.jsx
+++ client/src/Routes/PageRoutes.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-// import ErrorRoutes from './ErrorRoutes';
-import LandingPage from '../pages/LandingPage';
+import ErrorRoutes from './ErrorRoutes';
+import LandingPage from '../pages/LandingPage/LandingPage'
 
 const PagesRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<LandingPage />} />
             <Route path='/home' element={<LandingPage />} />
-            {/* <Route path="*" element={<ErrorRoutes />} /> */}
+
+            <Route path="*" element={<ErrorRoutes />} />
         </Routes>
     );
 }
