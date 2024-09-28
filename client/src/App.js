1590b84c8a933430e81438e047892039361b04c6
saistar4321 <72140281+saistar4321@users.noreply.github.com>
2024-09-28 10:31:58 +0530

    UpdatedChanges

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,14 @@
-import { useState } from 'react';
-import ErrorBoundary from './errorBoundary';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import MainPage from '../src/components/Homepage/mainPage';
-import Carousel from '../src/components/Homepage/Carousel/carousel';
-import Intro from '../src/components/Homepage/Hero/introBox.jsx';
-import Navbar from '../src/components/Homepage/Navbar/navbar.jsx';
+import React from 'react';
+import ExamHallAllocation from './ExamHallAllocation';
+
 
 function App() {
-  
-
-  return (
-    <>
-    <ErrorBoundary>
-    <BrowserRouter>
-    <Routes>
-      <Route index path='/' element={<MainPage/>} />
-      <Route path='/carousel' element={<Carousel/>} />
-      <Route path='/intro' element={<Intro/>} />
-      <Route path='/navbar' element={<Navbar/>} />
-    </Routes>
-    </BrowserRouter>
-    </ErrorBoundary>
-        
-    </>
-  )
+    return (
+        <div>
+            <ExamHallAllocation />
+            {/* Other components */}
+        </div>
+    );
 }
 
-export default App
+export default App;
