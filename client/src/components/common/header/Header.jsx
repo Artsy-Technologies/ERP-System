22e3346cb53bf4c6fbae9fa44643c7b4f32fcfd7
Prithwi <prithwi.online11@gmail.com>
2024-10-21 23:12:49 +0530

    header overflow fix

--- client/src/components/common/header/Header.jsx
+++ client/src/components/common/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <header style={{height:"4rem" , position:"sticky",top:"0",left:"0",zIndex:"10" }} className="header">
+    <header style={{height:"4rem", position:"sticky",top:"0",left:"20",zIndex:"4" }} className="header">
       <div className="search-bar"><i className="fas fa-search"></i>
         <input type="text" id="search bar" placeholder="Search..." />
       </div>
