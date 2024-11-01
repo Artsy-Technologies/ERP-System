bf2133d2c054a9e29d8a800ad9de27c522c8e665
Monika Kumari <monikalonewolf35@gamil.com>
2024-11-01 18:03:47 +0530

    duebook update

--- client/src/components/Students/DigitalLibrary/Duebook.jsx
+++ client/src/components/Students/DigitalLibrary/Duebook.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "./libraryApp.css";
 
-const DueBookCard = ({ book, onClose }) => {
+const DueBookCard = ({ book, onClose, borrowDuration=0 }) => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [isChecked, setIsChecked] = useState(true);
-  const [borrowDuration, setBorrowDuration] = useState(7);
+  // const [borrowDuration, setBorrowDuration] = useState(7);
 
   const handleBorrowClick = () => {
     setIsPopupOpen(true);
@@ -28,7 +28,7 @@ const DueBookCard = ({ book, onClose }) => {
             {book.author}, <p style={{ fontSize: "10px" }}> {book.year}</p>
           </p>
         </div>
-        <p  className= "text-center" style={{ fontSize: "20px" }}>{borrowDuration*1000} days</p>
+        <p  className= "text-center" style={{ fontSize: "20px" }}>{borrowDuration} days</p>
 
         {book.format ? (
           <p className= "text-center" style={{width: "140px", fontSize: "20px", }}>
@@ -96,7 +96,7 @@ const DueBookCard = ({ book, onClose }) => {
                 </div>
               </div>
 
-              <div className="total-amount text-start">TOTAL: {5* book.penality_fee}</div>
+              <div className="total-amount text-start">TOTAL: {(borrowDuration)* book.penality_fee}</div>
 
               <div className="dl-save-card mt-3">
                 <input
--- client/src/components/Students/DigitalLibrary/Openbookpopup.jsx
+++ client/src/components/Students/DigitalLibrary/Openbookpopup.jsx
@@ -10,6 +10,7 @@ import {
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"; 
 import Swal from "sweetalert2";
+import DueBookCard from "./Duebook";
 
 const BookPopup = ({ book,onUpdateBookStatus,onClose }) => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -43,6 +44,10 @@ const BookPopup = ({ book,onUpdateBookStatus,onClose }) => {
     // }, borrowDuration *24*60*60* 1000); // This is to simulate the borrow time
     
     setIsPopupOpen(false);
+
+    <DueBookCard
+    book={book}
+    borrowDuration = {borrowDuration}/>
   };
 
 
