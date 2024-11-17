332f9d5bbf78de0b9b7c6b8e024285eb7319babe
Vasudev <143821358+vasudevb25@users.noreply.github.com>
2024-11-17 09:50:08 +0530

    Facultyprofile & virtualclass PR

---
+++ client/src/components/Virtualclass/components/action.jsx
@@ -0,0 +1,12 @@
+import './action.css';
+
+const ActionButtons = () => {
+  return (
+    <div className="action-buttons">
+      <button className="btn">Upload Video</button>
+      <button className="btn">Schedule Meeting</button>
+    </div>
+  );
+};
+
+export default ActionButtons;
