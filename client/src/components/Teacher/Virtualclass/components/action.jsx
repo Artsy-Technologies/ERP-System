0d7e82cf13f0338fd39ca45c001a9c58bac3bc3b
Prithwi <prithwi.online11@gmail.com>
2024-11-27 21:51:18 +0530

    fixes

--- client/src/components/Teacher/Virtualclass/components/action.jsx
+++ client/src/components/Teacher/Virtualclass/components/action.jsx
@@ -3,8 +3,8 @@ import './action.css';
 const ActionButtons = () => {
   return (
     <div className="action-buttons">
-      <button className="btn">Upload Video</button>
-      <button className="btn">Schedule Meeting</button>
+      <button className="action-btn">Upload Video</button>
+      <button className="action-btn">Schedule Meeting</button>
     </div>
   );
 };
